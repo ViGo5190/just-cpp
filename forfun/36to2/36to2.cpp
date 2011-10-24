d6dd04b355449a4c6e119d4da9a29c433f49bb80
Stanislav Gumeniuk <vigo@vigo.su>
2011-10-24 11:12:32 +0400

    123

--- forfun/36to2/36to2.cpp
+++ forfun/36to2/36to2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-
+#include <stdint.h>
+#include <cmath>
 using namespace std;
 
-int main(){
+int64_t mypow(int64_t val, int64_t P)
+    {
+    int64_t r = val;
+    for (int64_t i=1; i<P; i++){
+        r = r*val;
+    }
+    return r;
+}
 
+
+int main(){
+    int64_t n;
+    cin >> n;
+    cout << mypow(2,n);
     return 0;
 }
