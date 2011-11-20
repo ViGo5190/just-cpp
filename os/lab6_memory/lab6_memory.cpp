44377f6483730b692aa358bf8ab6a80f160bd4e8
Stanislav Gumeniuk <vigo@vigo.su>
2011-11-21 01:16:31 +0400

    1

--- os/lab6_memory/lab6_memory.cpp
+++ os/lab6_memory/lab6_memory.cpp
@@ -1,15 +1,34 @@
-#include <istream>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
+int static const N = 1024;
 
-
+char memory[N];
 
 struct myMCB {
-    int n;
+    unsigned int size;
+    myMCB *start;
+    myMCB *end;
+};
+
+void* my_alloc(size_t size){
+    return 0;
 }
 
+void my_delete(void * ptr){
+
+    return;
+}
 
 int main(){
-
+    fill(memory,memory+N,0);
+    cout << &memory[0] << "-"<<&memory[N+1];
+    char* q = &memory[0];
+    *q = '1';
+    cout << endl<<q;
+    
+    cout << endl<<"end";  
     return 0;
 }
