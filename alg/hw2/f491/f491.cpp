aefb168fc29421c12d85f73b13ec712b4494c469
ViGo5190 <vigo@vigo.su>
2011-12-13 20:53:25 +0400

    lets try

--- alg/hw2/f491/f491.cpp
+++ alg/hw2/f491/f491.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
 
 
 using namespace std;
 
-void solve(const int &n, const int &k, const string &str){
-    short dp[50001][5001];
+int mysolve(){
+    int n,k;
+    string str;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    in >> n >> k;
+    in >> str;
+    
+    short *dp[5001];
+    for (int i = 0; i < 5001; i++) {
+        short d[5001];
+        dp[i] = d;
+    }
+    
     for ( int i = 1; i < n; ++i ){
         for ( int j = 0; j < n - i; ++j ) {
             int l = j + i;
             dp[j][l] = 1e4;
             if ( str[j] == str[l] ) {
                 dp[j][l] = dp [j+1][l-1];
-            } else if ( dp[j+1][l-1]+1<= k){
+            } else if ( dp[j+1][l-1]+1 <= k){
                 dp[j][l] = dp[j+1][l-1] + 1;
             }
         }
@@ -26,19 +38,13 @@ void solve(const int &n, const int &k, const string &str){
             }
         }
     }
-    cout << res;
+    out << res;
+    
+    return 0;
 }
             
 
 int main (){
-    int n,k;
-    string str;
-    ifstream in("input.txt");
-    ofstream out("output.txt");
-    in >> n >> k;
-    in >> str;
-
-    solve(n,k,str);
-    cout << str;
+    mysolve();
     return 0;
 }
