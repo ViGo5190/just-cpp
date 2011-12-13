ebc346bd0a462f2a68c0585f47eb36d90c5a0a24
ViGo5190 <vigo@vigo.su>
2011-12-13 17:25:56 +0400

    levenstain dist

--- alg/hw2/d1791/d1791.cpp
+++ alg/hw2/d1791/d1791.cpp
@@ -5,34 +5,39 @@
 #include <string>
 
 using namespace std;
-
-int damerauLevenshteinDistance( const std::vector<char>& a, const std::vector<char>& b )
-{
-    const int a_size = static_cast<int>( a.size() );
-    const int b_size = static_cast<int>( b.size() );
-    const int INF = a_size + b_size;
-    std::vector<std::vector<int> > H( a.size()+2, std::vector<int>( b.size()+2 ) );
-    H[0][0] = INF;
-    for ( int i = 0; i <= a_size; ++i ) { H[i+1][1] = i; H[i+1][0] = INF; }
-    for ( int j = 0; j <= b_size; ++j ) { H[1][j+1] = j; H[0][j+1] = INF; }
-     
-    const int alphabet_size = std::numeric_limits<char>::max();
-    std::vector<char> DA( alphabet_size );
-    for ( int d = 0; d < alphabet_size; ++d ) DA[d] = 0;
-    for ( int i = 1; i <= a_size; ++i ) {
-        size_t DB = 0;
-        for ( int j = 1; j <= b_size; ++j ) {
-            const int i1 = DA[ b[j-1] ];
-            const int j1 = DB;
-            const int d = (a[i-1]==b[j-1]) ? 0 : 1;
-            if ( d == 0 ) DB = j;
-            H[i+1][j+1] = std::min( 
-                std::min(H[i][j]+d, H[i+1][j]+1), 
-                std::min(H[i][j+1]+1, H[i1][j1] + (i-i1-1) + 1 + (j-j1-1) ) );
-        }
-        DA[ a[i-1] ] = i;
+int levenDist ( const string &a , const string &b ){
+    if ( a == b ){
+        return 0;
     }
-    return H[a.size()+1][b.size()+1];
+    
+    const int a_size = a.size();
+    const int b_size = b.size();
+    
+    if ( a_size == 0 ) {
+        return b_size;
+    } else if ( b_size == 0 ){
+        return a_size;
+    }
+
+
+    const int INF = a_size + b_size;
+    vector < vector<int> > T( a_size + 2, vector<int> ( b_size + 2 ) );
+    T[0][0] = 0;
+    for ( int i = 0; i <= a_size; ++i ) { T[i][0] = i;  }
+    for ( int j = 0; j <= b_size; ++j ) { T[0][j] = j; }
+
+    int cost, above_cell,left_cell,dig_cell;
+    for ( int i = 1; i <= a_size; ++i ){
+        for ( int j = 1; j <= b_size; ++j) {
+            cost = a[i-1] == b[j-1] ? 0 : 1;        
+            above_cell = T[i-1][j];
+            left_cell = T[i][j-1];
+            dig_cell = T[i-1][j-1];
+            T[i][j] = min( min( above_cell + 1 , left_cell + 1 ) , dig_cell + cost );
+        }
+    }
+
+    return T[a_size][b_size];
 }
 
 int main(){
@@ -44,7 +49,6 @@ int main(){
 
     in >> firstString;
     in >> secondString; 
-    cout << firstString;
-    cout << endl << secondString;    
+    out << levenDist( firstString , secondString );    
     return 0;
 }
