952cebd04a2a0441b73fa728c0e08eb913be7a17
ViGo5190 <vigo@vigo.su>
2011-12-19 00:19:16 +0400

    some done

--- os/lab6_memory/lab6_memory.cpp
+++ os/lab6_memory/lab6_memory.cpp
@@ -10,37 +10,74 @@ class MCB{
 	public:
 	size_t magicStart;
 	size_t magicEnd;
-	int size;
+	size_t size;
+	int index;
 	MCB **ptr;
 	
 	MCB *next;
 	MCB *prev;
 	
 	MCB(){
-		//index = 0;
-		//ptr = 1024;
-		//size = 0;
 		next = (MCB*)ram;
-		size = 1;
+		size = 0;
+		next = NULL;
+		index = 0;
 	}	
 	
 };
 
-static MCB* mem = (MCB*)ram;
+//static MCB* mem = (MCB*)ram;
 
 
 size_t lastMCB = sizeof(MCB);
 
-int my_alloc (size_t user_size){
-	MCB** iterator = &mem;
-	cout << iterator.->size;
+void* my_alloc (size_t user_size){
+	MCB* mem =  (MCB*)ram;
+	MCB* prev = mem;
+	
+	cout << "sss" << user_size << endl;
+	while ((mem->next)&&(mem->index > 0)){
+		prev = mem;
+		mem = mem->next;
+		cout << " q " << mem   << " ! " << prev<< endl;
+	}
+
+	
+	
+	
+	mem->next = (MCB*)(mem + sizeof(mem) + mem->size);
+	
+	if (prev->index>0){
+		mem->prev = prev;
+		prev->next = mem;
+		mem -> index = prev->index + 1;
+		
+			
+	} else {
+		mem->index = 1;
+	}
+	mem->size = user_size;
+	cout << prev->index << " ~" <<mem->index <<  "!!" << mem->next << " prev = " << mem->prev <<endl;
+	
+	return mem;
+
+	
 }
 
 int main(void){
 	char a = '7';
 	char* b;
 	b = (char*)&a;
-	cout << b << endl;
+	void *q[5];
+
+	q[0] =  my_alloc(5);
+	q[1] =  my_alloc(2);
+	q[2] =  my_alloc(3);
+	q[3] =  my_alloc(10);
+	//cout << b << endl;
+	//cout << ram << endl;
+	cout << q[0] << " ~"<< q[1]<< " ~"<< q[2]<< " ~"<< q[3] << endl;
+
 
 	
 
