fc85f6098c80163f6de73b7016438008a6ce14eb
ViGo5190 <vigo@vigo.su>
2012-01-10 22:40:42 +0400

    map, fill, delete

--- os/lab6_mem/lab6_mem.cpp
+++ os/lab6_mem/lab6_mem.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <algorithm>
+#include <iomanip>
 #include "lab6_mem.hpp"
 
 
@@ -12,24 +13,34 @@ using namespace std;
 
 
 
-
-
-
-
-
-
 int main(void) {
-	cout << "start = " <<&ram;
-	cout << endl;
+	//cout << "start = " <<&ram;
+	//cout << endl;
 
 	void *q[5];
  
 	q[0] = my_alloc(5);
 	cout << "q0 = " << q[0] << endl;
-	q[1] = my_alloc(5);
+	q[1] = my_alloc(7);
 	cout << "q1 = " << q[1] << endl;
-	q[2] = my_alloc(5);
+	q[2] = my_alloc(3);
 	cout << "q2 = " << q[2] << endl;
-	q[3] = my_alloc(5);
+	q[3] = my_alloc(40);
 	cout << "q3 = " << q[3] << endl;
+
+	my_fill(q[1], 'w');
+	cout << "! " << (char*)q[1] << endl;
+
+	my_fill(q[2], 'q');
+	cout << "! " << (char*)q[2] << endl;
+
+	my_delete(q[1]);
+
+	
+	cout << "! " << (char*)q[1] << endl;
+
+	
+	cout << "! " << (char*)q[2] << endl;
+
+	print_map();
 }
--- os/lab6_mem/lab6_mem.hpp
+++ os/lab6_mem/lab6_mem.hpp
@@ -2,6 +2,8 @@
 
 char ram[MAX];
 
+
+
 class MCB {
 	public:
 	size_t size;
@@ -20,37 +22,83 @@ void * my_alloc (size_t user_data_size) {
 	MCB* prev = mem;
 
 	if (mem->next == NULL) {
-		//mem = new MCB();
+		std::fill( (char*)ram , (char*)ram + MAX, 0);
 		mem->next = (MCB*)((char*)mem + sizeof(MCB));
-		std::cout << "Created system MCB" << std::endl;
-		std::cout << "size of MCB = " << sizeof(MCB) <<std::endl;
-		//cout << (MCB*)(ram + MAX) << endl;
-		std::cout << "Max size = " <<(MCB*)((char*)mem + MAX) << std::endl;
-		std::cout << "Next block= " << mem->next << std::endl;
+		mem->size=0;
+		//std::cout << "Created system MCB" << std::endl;
+		//std::cout << "size of MCB = " << sizeof(MCB) <<std::endl;
+		//std::cout << "Max size = " <<(MCB*)((char*)mem + MAX) << std::endl;
+		//std::cout << "Next block= " << mem->next << std::endl;
 	}
-	std::cout << " --- " << std::endl;
+	//std::cout << " --- " << std::endl;
 	while (mem->next!=NULL) {
 		prev = mem;
 		//cout << "next is available" <<endl;
 		if ((MCB*)((char*)mem+sizeof(MCB) + user_data_size) > ((MCB*) (ram + MAX))) {
 			return NULL;
 		}
-
+		
+		if ((mem->next!=NULL) && (mem->size == 0) && ((char*)(mem->next) - (char*)(mem) - sizeof(MCB) >= user_data_size)) {
+			break;
+		}
+		
 		mem = mem->next;
 		//mem->prev = prev;
 		
 	}
 	mem->next = (MCB*)((char*)mem + sizeof(MCB) + user_data_size);
-	std::cout << "This blosk is = " <<   mem << " " << prev << " "  << mem->next << std::endl;
-	
+	mem->size = user_data_size;
+	//std::cout << "This blosk is = " <<   mem << " " << prev << " "  << mem->next << std::endl;
+	//std::cout << "Between " << (char*)(mem->next) - (char*)(mem) - sizeof(MCB)<< std::endl;
 
-	return mem;
+	return (char*)mem+sizeof(MCB);
 
 
 
 }
 
+void my_fill(void * ptr, char c) {
+	MCB* mem = (MCB*) ((char*)ptr-sizeof(MCB));
+	if (mem->size == 0) {
+		return;
+	}
+	std::fill( (char*)ptr , (char*) ptr + mem->size , c);
+}
 
-void * my_delete () {
+void * my_delete (void * ptr) {
+	MCB* mem = (MCB*) ((char*)ptr-sizeof(MCB));
+	my_fill(ptr, 0);
+	mem->size=0;
+	if (mem->next->next==NULL) {
+		mem->next=NULL;
+	}
+	//std::cout << mem << " " << mem->size << " " << mem->next <<std::endl;
+
 	
 }
+
+
+
+
+void print_map() {
+	MCB* mem = (MCB*)ram;
+	
+	while (mem->next!=NULL) {
+			
+
+		std::cout << "MCB: " << mem << " size: ";
+		std::cout << std::setw(3) << mem->size;
+		std::cout << " data start at: " << (void*)((char*)mem+sizeof(MCB) );
+		
+		std::cout << std::endl;	
+		mem = mem->next;
+	}
+
+	for (int i =0 ; i < MAX; i++) {
+		std::cout << ram[i];
+		
+	}
+	std::cout << std::endl;
+
+}
+
