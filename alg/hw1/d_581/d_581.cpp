b7bed37bef32d5ae765f996fa94be5d82f07b51b
ViGo5190 <vigo@vigo.su>
2012-01-16 01:16:40 +0400

    task d done

--- alg/hw1/d_581/d_581.cpp
+++ alg/hw1/d_581/d_581.cpp
@@ -32,6 +32,7 @@ void printsh (shelter * a,int size) {
 }
 
 int bin_search(shelter * arr, int pos, int size) {
+	//cout << "bin";
 	int low = 0;
 	int hight = size - 1;
 	int med;
@@ -42,7 +43,8 @@ int bin_search(shelter * arr, int pos, int size) {
 	}
 	
 	while (  hight - low > 0 ) {
-		med = (hight-low)/2;
+		//med = (hight-low)/2;
+		med = low + ((hight - low)>>1);
 		if (arr[med].position < pos) {
 			low = med + 1;
 		} else if (arr[med].position > pos) {
@@ -55,19 +57,21 @@ int bin_search(shelter * arr, int pos, int size) {
 	} else {
 		return hight;
 	}
-	return 0;
+	//return 0;
 }
 
-void quick_sort(shelter * arr, int left, int right, int size) {
+void quick_sort(shelter * arr, int left, int right) {
 	int low = left;
-	int hight = right;
-	int x = arr[size/2].position;
+	int hight = right - 1;
+	//int x = arr[size/2].position;
+	int x = arr[low + rand()%(right - left)].position;
+	//cout << x << endl;
 	int ti, tp;
 	//printsh(arr, size);
 	do {
 		while (arr[low].position < x) low++;
 		while (arr[hight].position > x) hight--;
-		if (low < hight) {
+		if (low <= hight) {
 			ti = arr[hight].index;
 			tp = arr[hight].position;
 
@@ -77,13 +81,15 @@ void quick_sort(shelter * arr, int left, int right, int size) {
 			arr[low].index = ti;
 			arr[low].position = tp;
 
-			low++; hight--;
+			low++; 
+			hight--;
 		}
 
 		
 	} while (low <= hight);
-	if (left < hight) quick_sort(arr,left, hight,size);
-	if (low < right) quick_sort(arr, low, right,size);
+	//cout<< low << " " << hight << " "<< left << " "<< right << endl;
+	if (left < hight) quick_sort(arr,left, hight + 1);
+	if (low < right - 1) quick_sort(arr, low, right);
 	return;
 }
 
@@ -95,7 +101,7 @@ int main(void) {
 	int k;
 	shelter *shelters;
 	int t;
-
+	srand(time(NULL));
 	ifstream in("input.txt");
 	in >> n;
 	village = new int[n];
@@ -113,13 +119,15 @@ int main(void) {
 
 	//printsh(shelters, k);
 	//cout << "q"<<endl;
-	quick_sort(shelters, 0, k-1,k);
+	//if ( (n = 4 ) && (k=2) )
+		quick_sort(shelters, 0, k);
 	//printsh(shelters, k);
-
+	//cout << "q" << endl;
 	//cout << shelters[bin_search(shelters, 1, n)].position;
-	for (int i = 0; i< n; i++) {
+	for (int i = 0; i < n; i++) {
 		cout << shelters[bin_search(shelters, village[i], k)].index << " ";
 		//cout << bin_search(shelters, village[i], k) << " ";
+		//cout << "w";
 	}
 
 
